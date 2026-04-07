3440fa70da69aa4190503df5c179bf1501500748
BrayanChiliquinga <chiliquingabrayan75@gmail.com>
2026-04-07 00:14:28 -0500

    Update app_poisson.py

--- app_poisson.py
+++ app_poisson.py
@@ -203,7 +203,7 @@ if len(freq_obs_valid) > 1:
             
         
     except:
-        st.warning("No hay suficientes datos válidos (freq esperada ≥ 5)")
+        st.warning("No hay suficientes datos válidos para determinar Chi-Cuadrado (freq esperada ≥ 5)")
     
     
     else:
